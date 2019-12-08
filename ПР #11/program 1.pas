9846980bc17e7cc0d0cdbe224f8802ef1add3c5b
Filin3 <43331414+Filin3@users.noreply.github.com>
2019-12-08 20:22:24 +0500

    Add files via upload

---
+++ ПР #11/program 1.pas
@@ -0,0 +1,19 @@
+﻿const fileName = '1.res';
+var res: file of integer;
+var i,n:integer;
+begin
+  assign(res, './' + fileName);
+  rewrite(res);
+  readln(n);
+  for i := 1 to n do begin
+    write(res, i);
+  end;
+  close(res);
+  
+  assign(res, './'  + fileName);
+  reset(res);
+  while eof(res) <> true do begin
+    read(res,n);
+    writeln(n);
+  end;
+end.
---
+++ ПР #11/program 2.pas
@@ -0,0 +1,44 @@
+﻿const fileName = '1.res';
+var resMain: file of integer;
+var resTemp: file of integer;
+var nRead: integer;
+
+begin
+  writeln('Befone: ');
+  assign(resMain, './' + fileName);
+  assign(resTemp, './RESTEMP.res');
+  reset(resMain);
+  rewrite(resTemp);
+  while eof(resMain) <> true do begin
+    read(resMain, nRead);
+    write(resTemp, nRead);
+    writeln(nRead);
+  end;
+  close(resMain);
+  close(resTemp);
+  
+  rewrite(resMain);
+  reset(resTemp);
+  while eof(resTemp) <> true do begin
+    read(resTemp, nRead);
+    write(resMain, nRead);
+  end;
+  close(resTemp);
+  reset(resTemp);
+  while eof(resTemp) <> true do begin
+    read(resTemp, nRead);
+    write(resMain, nRead * 10);
+  end;
+  
+  close(resMain);
+  close(resTemp);
+  
+  Erase(resTemp);
+ 
+  writeln('After: ');
+  reset(resMain);
+  while eof(resMain) <> true do begin
+    read(resMain, nRead);
+    writeln(nRead);
+  end; 
+end.
